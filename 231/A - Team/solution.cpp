441d33ff2411cf9be2f2b73c9712e2dca915af02
Janvii07 <mahourjanvi493@gmail.com>
2026-07-19 21:25:10 +0530

    Add Team [A] from Codeforces

---
+++ 231/A - Team/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin>>n;
+    int ans=0;
+ 
+    for(int i=0;i<n;i++){
+        int a,b,c;
+        cin>>a;
+        cin>>b;
+        cin>>c;
+ 
+        if(a+b+c>=2){
+            ans+=1;
+        }
+ 
+    }
+    
+    cout<<ans;
+    
+ 
+ 
+}
